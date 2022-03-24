561abfe91d0bb4eac62963f4735ef75b40346e7b
Flavio Vieira <flavio.vl@gmail.com>
2022-03-24 09:40:14 -0300

    #55: add product endpoint

--- PartyRental/urls.py
+++ PartyRental/urls.py
@@ -2,13 +2,14 @@ from django.contrib import admin
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from users import urls as user_routes
+from product import urls as product_routes
 
 router = DefaultRouter()
 router.registry.extend(user_routes.router.registry)
+router.registry.extend(product_routes.router.registry)
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(router.urls)),
-    # path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- product/models.py
+++ product/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
-# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    description = models.CharField(max_length=100, unique=True)
+    weight = models.IntegerField('peso')
+    active = models.BooleanField('ativo', default=True)
+    price = models.DecimalField('preço', max_digits=7, decimal_places=2)
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+class Category(models.Model):
+    name = models.CharField(max_length=20, unique=True)
+    description = models.CharField(max_length=100, unique=True)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
