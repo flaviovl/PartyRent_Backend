9ac25e785020ce25bb6877399c5d2145c01e3d6a
Flavio Vieira <flavio.vl@gmail.com>
2022-03-24 08:42:55 -0300

    #54: add user registation endpoint

--- users/views.py
+++ users/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import UserSerializer
+from .models import User
+# from rest_framework import permissions
 
-# Create your views here.
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint signup.
+    """
+    queryset = User.objects.all().order_by('-date_joined')
+    serializer_class = UserSerializer
+    # permission_classes = [permissions.IsAuthenticated]
+    # permission_classes = [permissions.IsAdminUser | CurrentUserOnly]
+
+
+# class ProfileViewSet(viewsets.ModelViewSet):
+#     """
+#     API endpoint profile.
+#     """
+#     queryset = Profile.objects.all()
+#     serializer_class = ProfileSerializer
+#     # permission_classes = [permissions.IsAuthenticated]
+
