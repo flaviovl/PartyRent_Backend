8a571a102ba58fffc07e23d8c8a3d9dea9f24c6d
Flavio Vieira <flavio.vl@gmail.com>
2022-03-24 09:52:02 -0300

    #55: add category endpoint

--- product/views.py
+++ product/views.py
@@ -1,9 +1,14 @@
 from rest_framework import viewsets
 
 from product.models import Product
-from product.serializers import ProductSerializer
+from product.serializers import ProductSerializer, CategorySerializer
 
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = CategorySerializer
