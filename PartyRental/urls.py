c42edfc9aeab67fb1618b0a351109e7eefaa251f
Flavio Vieira <flavio.vl@gmail.com>
2022-03-24 10:58:54 -0300

    #56: add shoppingcart views and urls

--- PartyRental/urls.py
+++ PartyRental/urls.py
@@ -3,10 +3,12 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 from users import urls as user_routes
 from product import urls as product_routes
+from shoppingcart import urls as cart_routes
 
 router = DefaultRouter()
 router.registry.extend(user_routes.router.registry)
 router.registry.extend(product_routes.router.registry)
+router.registry.extend(cart_routes.router.registry)
 
 
 urlpatterns = [
