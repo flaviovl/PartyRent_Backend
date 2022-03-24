aa3ba5db7ed347c0c0b31d328d6da13c924b9d66
Flavio Vieira <flavio.vl@gmail.com>
2022-03-24 11:49:50 -0300

    #docs: add api documentation swagger- drf

--- PartyRental/urls.py
+++ PartyRental/urls.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
 from rest_framework.routers import DefaultRouter
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
 from users import urls as user_routes
 from product import urls as product_routes
 from shoppingcart import urls as cart_routes
@@ -10,8 +13,27 @@ router.registry.extend(user_routes.router.registry)
 router.registry.extend(product_routes.router.registry)
 router.registry.extend(cart_routes.router.registry)
 
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Party Rental API",
+        default_version='v1',
+        description="A API e voltada a realização de aluguel para artigos diversos para festas.",
+        contact=openapi.Contact(email="contact@partyrental.local"),
+        license=openapi.License(name="FGA License"),
+    ),
+    public=True,
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include(router.urls)),
 ]
+
+# swagger
+urlpatterns += [
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+]
+
