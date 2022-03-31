1441a5ce8052f1f18fe86593d21d9f7c48ae6983
mateusbrandao <mateus.brandaoteixeira@gmail.com>
2022-03-31 17:46:49 -0300

    add categorys painel admin django

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 
 # Register your models here.
-from product.models import Product
+from product.models import Product,Category
 
 
 @admin.register(Product)
@@ -9,3 +9,9 @@ class Product(admin.ModelAdmin):
     list_display = ('name', 'description', 'weight', 'active', 'price','category')
     list_filter = ('name', 'description', 'weight', 'active', 'price','category')
     search_fields = ('name', 'description', 'weight', 'active', 'price','category')
+
+@admin.register(Category)
+class Category(admin.ModelAdmin):
+    list_display = ('name', 'description')
+    list_filter = ('name', 'description')
+    search_fields = ('name', 'description')
