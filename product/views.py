f40890266671b3d6db5c5fa253b7f1354071ce6a
Victor Rayan Ferreira <victor.rayan378@gmail.com>
2022-04-04 21:20:40 -0300

    testing authentication
    its just a test with rest authentication

--- product/views.py
+++ product/views.py
@@ -2,11 +2,14 @@ from rest_framework import viewsets
 
 from product.models import Product
 from product.serializers import ProductSerializer, CategorySerializer
-
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class CategoryViewSet(viewsets.ModelViewSet):
