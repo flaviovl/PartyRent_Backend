3a35dea7a8f7e4648b4bc573bac7951528c15bcd
Victor Rayan Ferreira <victor.rayan378@gmail.com>
2022-04-07 20:00:13 -0300

    Update cart with authentication

--- shoppingcart/views.py
+++ shoppingcart/views.py
@@ -3,14 +3,17 @@ from rest_framework import viewsets
 
 from .models import ShoppingCart, CartItem, RentalOrder
 from .serializers import ShoppingCartSerializer, CartItemSerializer, RentalOrderSerializer
+from rest_framework.authentication import BasicAuthentication
 from rest_framework.permissions import IsAuthenticated
 
 
 
 class ShoppingCartViewSet(viewsets.ModelViewSet):
-    permission_classes = (IsAuthenticated,)
+    
     queryset = ShoppingCart.objects.all()
     serializer_class = ShoppingCartSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
 
 class CartItemViewSet(viewsets.ModelViewSet):
