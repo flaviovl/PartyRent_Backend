7e656b9036e4cc9b692eb2037339eac3a671edf0
Victor Rayan Ferreira <victor.rayan378@gmail.com>
2022-04-07 19:58:03 -0300

    Update view with authentication

--- review/views.py
+++ review/views.py
@@ -2,9 +2,13 @@ from rest_framework import viewsets
 
 from .models import Review
 from .serializers import ReviewSerializer
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    authentication_classes = [BasicAuthentication]
+    permission_classes = [IsAuthenticated]
 
