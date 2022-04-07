0b586303cc486fd5c216c1346037ad866ad9d025
Victor Rayan Ferreira <victor.rayan378@gmail.com>
2022-04-07 20:04:37 -0300

    Importing libraries users

--- users/views.py
+++ users/views.py
@@ -1,7 +1,9 @@
 from rest_framework import viewsets
 from .serializers import UserSerializer
 from .models import User
-# from rest_framework import permissions
+
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.permissions import IsAuthenticated
 
 
 class UserViewSet(viewsets.ModelViewSet):
