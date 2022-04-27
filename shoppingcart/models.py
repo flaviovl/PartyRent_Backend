c7ed80a7de2e90e072b5558b4ae44c2b556b5fcf
Flavio Vieira <flavio.vl@gmail.com>
2022-04-27 19:32:57 -0300

    :refactor: model shoppingcart
    - add class status choices and atribute status
    - add (@property) func amount : return total price of all items in shopping cart
    - remove class order

--- shoppingcart/models.py
+++ shoppingcart/models.py
@@ -1,38 +1,70 @@
 from django.db import models
-from users.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 from product.models import Product
+from users.models import User
 
 
 class ShoppingCart(models.Model):
-    client = models.ForeignKey(User, verbose_name='Cliente', on_delete=models.SET_NULL, null=True)
-    is_ordered = models.BooleanField('Pedido', default=False)
-    date_ordered = models.DateTimeField('Data', auto_now=True)
+    class StatusShoppingCart(models.IntegerChoices):
+        PENDING = 1, "Carrinho de Compras"
+        CONFIMED = 2, "Orçamento Confirmado"
+        ORDER = 3, "Ordem de Serviço"
+        CANCELED = 7, "Cancelado"
+    
+    client = models.ForeignKey(
+        User,
+        verbose_name="Cliente",
+        related_name="client_shoppingcarts",
+        on_delete=models.RESTRICT
+    )
+    status = models.IntegerField(
+        verbose_name="Status",
+        choices=StatusShoppingCart.choices,
+        default=StatusShoppingCart.PENDING)
+    date_created = models.DateTimeField("Data", auto_now=True)
 
     class Meta:
-        verbose_name_plural = "Carts"
+        verbose_name_plural = "Carrinhos de compras"
+        verbose_name = "Carrinho de compra"
 
-
-class CartItem(models.Model):
-    product_id = models.ForeignKey(Product, verbose_name='Produto', on_delete=models.SET_NULL, null=True)
-    cart_id = models.ForeignKey(ShoppingCart, verbose_name='Carinho de Compras', on_delete=models.SET_NULL, null=True)
-    quantity = models.IntegerField('Quantidade', null=True, default=0)
-    create_date = models.DateTimeField('Data', auto_now_add=True)
-
-    class Meta:
-        verbose_name_plural = "Order Itens"
-
-
-class RentalOrder(models.Model):
-    client = models.ForeignKey(User, verbose_name='Cliente', on_delete=models.CASCADE)
-    cart_id = models.ForeignKey(ShoppingCart, verbose_name='Carinho de Compras', on_delete=models.SET_NULL, null=True)
-    rental_date = models.DateTimeField('Data Locação', auto_now_add=True, auto_now=False)
-    return_date = models.DateTimeField('Data Retorno', auto_now_add=True, auto_now=False)
-    amount = models.DecimalField('Total', max_digits=100, decimal_places=2)
-    id_paid = models.BooleanField('Pago', default=False)
-
-    class Meta:
-        verbose_name_plural = "Rental Order"
+    @property 
+    def amount(self):
+        qs = self.products.all()
+        return sum(product.sub_total for product in qs)
+        
+        # usando funções django
+        # qs = self.products.all().aggregate(total=models.Sum(F("quantity") * F("price")) )                     
+        # return qs["total"]
 
     def __str__(self):
-        return self.cart_id
+        return f"{self.id}"
 
+class CartItem(models.Model):
+    cart = models.ForeignKey(
+        ShoppingCart,
+        verbose_name="carinho de compras",
+        related_name="products",
+        on_delete=models.SET_NULL, null=True
+    )
+    product = models.ForeignKey(
+        Product,
+        verbose_name="produto",
+        related_name="+",
+        on_delete=models.SET_NULL,
+        null=True,
+    )
+    price = models.DecimalField(max_digits=8, decimal_places=2)
+    quantity = models.IntegerField("quantidade", null=True, default=0)
+    create_date = models.DateTimeField("data", auto_now_add=True)
+
+    class Meta:
+        verbose_name_plural = "itens do carrinho"
+        ordering = ("pk",)
+    
+    def __str__(self):
+        return f"{self.id}"
+    
+    @property
+    def sub_total(self):
+        return self.price * self.quantity
