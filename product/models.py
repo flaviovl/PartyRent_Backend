6f99974b6931491ec14e23f16f04f6aa0bb61995
Flavio Vieira <flavio.vl@gmail.com>
2022-04-27 22:45:01 -0300

    refactor: model products category
    - Class Product:
        - add attribute:
            - slug (to create slug url)
            - image (to show image)
            - thumbnail (to show thumbnail)
    
        - add method:
            - get_absolute_url (to create url)
            - get_image_url (to get image url)
            - get_thumbnail_url (to get thumbnail url)
            - make_thumbnail (to create thumbnail)
            - save (to auto create slug whith slugigy)
            @property: (method as attribute)
                - image_url (to get image url)
                - thumbnail_url (to get thumbnail url)
                - endpoint_pk (to get endpoint pk)
                - endpoint_slug (to get endpoint slug)
                - path_pk (to get path pk)
                - path_slug (to get path slug)
    
    - Class Category:
        - add attribute slug (to create slug url)
        - add method get_absolute_url (to create url)

--- product/models.py
+++ product/models.py
@@ -1,35 +1,115 @@
+from io import BytesIO
+
+from django.conf import settings
+from django.core.files import File
 from django.db import models
+from django.urls import path
+from django.utils.text import slugify
+from PartyRental.settings import BASE_DEBUG_URL as baseURL
+from PIL import Image
+
+
+class Category(models.Model):
+    name = models.CharField('nome', max_length=50)
+    slug = models.SlugField(max_length=100, unique=True, blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'categoria'
+        verbose_name_plural = 'categorias'
+        ordering = ('id',)
+
+    @property
+    def get_absolute_url(self):
+        return f'/{self.slug}/'
+
+    def __str__(self):
+        return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super(Category, self).save(*args, **kwargs)
 
 
 class Product(models.Model):
-    name = models.CharField('nome', max_length=20, unique=True)
-    description = models.CharField('descrição', max_length=100, unique=True)
-    weight = models.IntegerField('peso')
-    active = models.BooleanField('ativo', default=True)
-    price = models.DecimalField('preço', max_digits=7, decimal_places=2)
-    teste = models.ImageField(upload_to='media/product_photo', blank=True, null=True)
     category = models.ForeignKey(
-        'Category',
-        verbose_name='categoria',
+        Category,
+        verbose_name='Categoria',
+        related_name='category_products',
         on_delete=models.SET_NULL,
         null=True,
         blank=True
     )
+    name = models.CharField('nome', max_length=50)
+    slug = models.SlugField(max_length=100, unique=True, blank=True, null=True)
+    description = models.TextField('descrição', blank=True)
+    price = models.DecimalField('preço', max_digits=6, decimal_places=2)
+    image = models.ImageField(upload_to="images/", default="images/no-image.png", blank=True, null=True)
+    thumbnail = models.ImageField(upload_to="thumbnail/", blank=True, null=True)
+    in_stock = models.BooleanField('estoque', default=True)
+    is_active = models.BooleanField('ativo', default=True)
+    created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ('name',)
+        verbose_name = 'produto'
+        verbose_name_plural = 'produtos'
+        ordering = ('-created',)
+        
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super(Product, self).save(*args, **kwargs)
 
-    def __str__(self):
-        return self.name
-
-
-class Category(models.Model):
-    name = models.CharField('nome', max_length=20, unique=True)
-    description = models.CharField('descrição', max_length=100, unique=True)
-
-    class Meta:
-        ordering = ('name',)
-
-    def __str__(self):
-        return self.name
+    @property
+    def endpoint_pk(self):
+        return self.get_absolute_pk_url()
     
+    @property
+    def endpoint_slug(self):
+        return self.get_absolute_slug_url()
+
+    @property
+    def path_pk(self):
+        return f"api/products/{self.pk}/"
+    
+    @property
+    def path_slug(self):
+        return f"api/products/{self.slug}/"
+
+    @property
+    def image_url(self):
+        return f'{baseURL}{self.image.url}' if self.image else ''
+    
+    @property
+    def thumbnail_url(self):
+        return self.get_thumbnail()
+
+    def get_absolute_slug_url(self):
+        return f'/{self.category.slug}/{self.slug}/'
+    
+    def get_absolute_pk_url(self):
+        return f'/{self.category.id}/{self.id}/'
+
+    def get_thumbnail(self):
+        if self.thumbnail:
+            return f'{baseURL}{self.thumbnail.url}'
+        if self.image:
+            self.thumbnail = self.make_thumbnail(self.image)
+            self.save()
+
+            return f'{baseURL}{self.thumbnail.url}'
+        else:
+            return ''
+
+    def make_thumbnail(self, image, size=(300, 200)):
+        img = Image.open(image)
+        img.convert('RGB')
+        img.thumbnail(size)
+
+        thumb_io = BytesIO()
+        img.save(thumb_io, 'JPEG', quality=85)
+
+        return File(thumb_io, name=image.name)
+
+    def __str__(self):
+        return self.name
