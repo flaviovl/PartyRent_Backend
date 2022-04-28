c3b067cb0c1ee40a9a924e68cd94c7c04da7fa68
Flavio Vieira <flavio.vl@gmail.com>
2022-04-27 22:49:08 -0300

    chore: register product and category in modeladmin

--- product/admin.py
+++ product/admin.py
@@ -1,17 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
-from product.models import Product,Category
+from product.models import Category, Product
 
+
+class ProductsInline(admin.TabularInline):
+    model = Product
+    extra = 1
 
 @admin.register(Product)
 class Product(admin.ModelAdmin):
-    list_display = ('name', 'description', 'weight', 'active', 'price','category')
-    list_filter = ('name', 'description', 'weight', 'active', 'price','category')
-    search_fields = ('name', 'description', 'weight', 'active', 'price','category')
+    list_display = ("category", "name", "description", "is_active", "price")
+    list_filter = ("category", "name", "description", "is_active", "price")
+    search_fields = ("category", "name", "description", "is_active", "price")
 
 @admin.register(Category)
 class Category(admin.ModelAdmin):
-    list_display = ('name', 'description')
-    list_filter = ('name', 'description')
-    search_fields = ('name', 'description')
+    list_display = ("name", "slug")
+    list_filter = ("name", "slug")
+    search_fields = ("name", "slug")
+    inlines = [ProductsInline]
