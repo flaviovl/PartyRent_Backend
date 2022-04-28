c06520ea5683f1aae58d5a63a00dbf92f634083d
Flavio Vieira <flavio.vl@gmail.com>
2022-04-27 22:05:03 -0300

    chore: register shoppingcart and cartitem in ModelAdmin
    - add list_display, list_filter and search_fields in ModelAdmin
    - add nested shoppingcart and cartitem in ModelAdmin"

--- shoppingcart/admin.py
+++ shoppingcart/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
-# Register your models here.
+from shoppingcart.models import CartItem, ShoppingCart
+
+
+class CartItemInline(admin.TabularInline):
+    model = CartItem
+    extra = 0
+    
+@admin.register(ShoppingCart)
+class ShoppingCart(admin.ModelAdmin):
+    list_display = ("client", "status", "date_created")
+    list_filter = ("client", "status", "date_created")
+    search_fields = ("id", "client")
+    inlines = [CartItemInline]
+
+@admin.register(CartItem)
+class CartItem(admin.ModelAdmin):
+    list_display = ("id", "cart", "product", "price", "quantity")
+    list_filter = ("cart", "product", "price", "quantity")
+    search_fields = ("id", "cart", "product")
