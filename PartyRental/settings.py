663efba077f5238152d8b473003e892f4db1beb9
Flavio Vieira <flavio.vl@gmail.com>
2022-04-27 23:27:31 -0300

    chore: add static url config  and simplejwt in apps

--- PartyRental/settings.py
+++ PartyRental/settings.py
@@ -8,7 +8,6 @@ from pathlib import Path
 
 import environ
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 ENVFILE_PATH = os.path.join(BASE_DIR, '.env')
@@ -27,6 +26,7 @@ SECRET_KEY = env_config('SECRET_KEY')
 DEBUG = env_config('DEBUG')
 
 ALLOWED_HOSTS = [env_config("ALLOWED_HOSTS")]
+BASE_DEBUG_URL = "http://127.0.0.1:8000"
 
 AUTH_USER_MODEL = 'users.User'
 # Application definition
@@ -37,13 +37,15 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework',
-    'drf_yasg',
-    'users',
-    'product',
-    'shoppingcart',
-    'review',
-    'corsheaders',
+    "rest_framework",
+    "rest_framework_simplejwt",
+    "drf_yasg",
+    "users",
+    "product",
+    "shoppingcart",
+    # "orders",
+    "review",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
@@ -145,19 +147,20 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.0/howto/static-files/
+STATIC_URL = "/static/"
+# STATIC_ROOT = BASE_DIR / "staticfiles"
+# STATICFILES_DIRS = [BASE_DIR / "static", ]
 
-STATIC_URL = '/static/'
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "media/"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
-    'DEFAULT_AUTENTICATION_CLASSES': (
-        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    "DEFAULT_AUTENTICATION_CLASSES": (
+        "rest_framework_simplejwt.authentication.JWTAuthentication",
     )
-
-
 }
