8e338847d46a0e09dc92c0e1cef9d5937dc0d6e9
Flavio Vieira <flavio.vl@gmail.com>
2022-04-27 22:50:20 -0300

    refactor: product and category serializeres
    - create a serializer:
        product(basic) with category name (use list product in cart url)
        product(full) with all fields and add avg_rating(use detail product url)
        category(basic)
        category with all field and products

--- product/serializers.py
+++ product/serializers.py
@@ -1,24 +1,78 @@
+from django.db.models import Avg
 from rest_framework import serializers
-from product.models import Product, Category
+from review.models import Review
+
+from product.models import Category, Product
 
 
-class ProductSerializer(serializers.HyperlinkedModelSerializer):
+# from review.serializers import BasicReviewSerializer
+class ProductBasicSerializer(serializers.ModelSerializer):
+    category = serializers.CharField(source="category.name")
+
     class Meta:
         model = Product
         fields = [
-            'url',
-            'id',
-            'name',
-            'description',
-            'weight',
-            'active',
-            'price',
-            'teste',
-            'category'
-        ]    
+            "id",
+            "name",
+            # "slug",
+            "price",
+            "category",
+        ]
+        # depth = 1
+class ProductSerializer(serializers.ModelSerializer):
+    category = serializers.CharField(source="category.name")
+    # product_reviews = BasicReviewSerializer(many=True)       # category_products = related_name
+    
+    # average_rating = serializers.SerializerMethodField()
 
+    class Meta:
+        model = Product
+        fields = [
+            "id",
+            "name",
+            # "slug",
+            "price",
+            "description",
+            "category",
+            # "average_rating",
+            # "product_reviews",
+            "endpoint_pk",
+            "endpoint_slug",
+            "path_pk",
+            "path_slug",
+            "image_url",
+            "thumbnail_url",
+        ]
+        # depth = 1
 
-class CategorySerializer(serializers.HyperlinkedModelSerializer):
+    # def get_average_rating(self, obj):
+    #     average = obj.product_reviews.aggregate(Avg('star_rating')).get('star_rating__avg')
+    #     if average is None:
+    #         return 0
+    #     return round(average, 2)
+class CategoryBasicSerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
-        fields = '__all__'
+        fields = [
+            "id",
+            "name",
+            "slug",
+        ]
+class CategorySerializer(serializers.ModelSerializer):
+    category_products = ProductSerializer(many=True)       # category_products = related_name
+
+    class Meta:
+        model = Category
+        fields = [
+            "id",
+            "name",
+            "slug",
+            "get_absolute_url",
+            "category_products"
+        ]
+
+
+# # class InvetorySerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Invetory
+#         fields = ['id', 'store_price', 'quantity']
