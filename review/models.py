8e5ab8890df8deb9f144e1ac052f01b13402a846
Flavio Vieira <flavio.vl@gmail.com>
2022-04-28 22:42:15 -0300

    style: code organization

--- review/models.py
+++ review/models.py
@@ -5,15 +5,26 @@ from users.models import User
 
 class Review(models.Model):
 
-    client = models.ForeignKey(User, verbose_name='Cliente', on_delete=models.SET_NULL, null=True)
-    product = models.ForeignKey(Product, verbose_name='Produto', on_delete=models.SET_NULL, null=True)
-    date = models.DateTimeField('Data', auto_now=True)
-    star_rating = models.PositiveIntegerField('Stars', default=5)
-    comment = models.TextField('Comentário', max_length=200)
+    client = models.ForeignKey(
+        User,
+        verbose_name="Cliente",
+        related_name="client_reviews",
+        on_delete=models.RESTRICT,
+    )
+    product = models.ForeignKey(
+        Product,
+        verbose_name="Produto",
+        related_name="product_reviews",
+        on_delete=models.RESTRICT,
+    )
+    date = models.DateTimeField("Data", auto_now=True)
+    star_rating = models.PositiveIntegerField("Stars", default=5)
+    comment = models.TextField("Comentário", max_length=200)
 
     class Meta:
         verbose_name = ("Comentário")
         verbose_name_plural = ("Comentarios")
+        ordering = ["id"]
 
     def __str__(self):
         return f"Cliente: {self.client.username} - Produto: {self.product.name}"
